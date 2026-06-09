2ab7c19e5f41df8f967e2598d0281f580add6ecc
bruhmeeme <kinjolxbag@gmail.com>
2026-06-09 20:15:04 +0530

    Add files via upload

---
+++ chess.evo/src/types.ts
@@ -0,0 +1,27 @@
+export interface ChessComGame {
+  url: string;
+  pgn: string;
+  time_control: string;
+  end_time: number;
+  rated: boolean;
+  tcn: string;
+  uuid: string;
+  initial_setup?: string;
+  fen?: string;
+  time_class: string;
+  rules: string;
+  white: {
+    rating: number;
+    result: string;
+    '@id': string;
+    username: string;
+    uuid: string;
+  };
+  black: {
+    rating: number;
+    result: string;
+    '@id': string;
+    username: string;
+    uuid: string;
+  };
+}
